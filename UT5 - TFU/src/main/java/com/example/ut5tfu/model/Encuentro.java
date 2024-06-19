d7e836503e57fe72ead29ab5e79dd9ae45c41df3
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-19 19:06:39 -0300

    Agrega id a Encuentro.

--- UT5 - TFU/src/main/java/com/example/ut5tfu/model/Encuentro.java
+++ UT5 - TFU/src/main/java/com/example/ut5tfu/model/Encuentro.java
@@ -4,12 +4,22 @@ import java.util.List;
 
 public class Encuentro {
 
+    private Integer id;
     private List<Equipo> equipos;
     private List<Integer> puntajes;
 
-    public Encuentro(List<Equipo> equipos, List<Integer> puntajes) {
+    public Encuentro(Integer id, List<Equipo> equipos, List<Integer> puntajes) {
+        this.id = id;
         this.equipos = equipos;
         this.puntajes = puntajes;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public List<Equipo> getEquipos() {
@@ -27,4 +37,5 @@ public class Encuentro {
     public void setPuntajes(List<Integer> puntajes) {
         this.puntajes = puntajes;
     }
+
 }
