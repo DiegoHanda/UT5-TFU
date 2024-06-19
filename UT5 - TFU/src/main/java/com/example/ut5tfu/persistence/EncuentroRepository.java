20f3205a4c538c7c7fa9533c4be606657459f2c3
Leandro Alfonso <leandroalfonsoporley@gmail.com>
2024-06-19 18:47:49 -0300

    Agrega Repository genérico y EncuentroRepository.

---
+++ UT5 - TFU/src/main/java/com/example/ut5tfu/persistence/EncuentroRepository.java
@@ -0,0 +1,6 @@
+package com.example.ut5tfu.persistence;
+
+import com.example.ut5tfu.model.Encuentro;
+
+public class EncuentroRepository extends Repository<Encuentro, String> {
+}
---
+++ UT5 - TFU/src/main/java/com/example/ut5tfu/persistence/Repository.java
@@ -0,0 +1,25 @@
+package com.example.ut5tfu.persistence;
+
+import java.util.*;
+
+public abstract class Repository<T, I> {
+
+    Map<I, T> entities = new HashMap<>();
+
+    public T save(T clase) {
+        return clase;
+    }
+
+    public List<T> getAll() {
+        return new ArrayList<>(this.entities.values());
+    }
+
+    public Optional<T> getById(I id) {
+        return Optional.of(this.entities.get(id));
+    }
+
+    public void delete(I id) {
+        this.entities.remove(id);
+    }
+
+}
