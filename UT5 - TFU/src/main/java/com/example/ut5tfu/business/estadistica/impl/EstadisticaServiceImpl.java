97f71118c3a90a0f7e7c0b9b45c53e43a2ef7687
Br1 <bruno.bugarin@correo.ucu.edu.uy>
2024-06-20 16:54:40 -0300

    Implementación del EstadisticaServiceImpl

--- UT5 - TFU/src/main/java/com/example/ut5tfu/business/estadistica/impl/EstadisticaServiceImpl.java
+++ UT5 - TFU/src/main/java/com/example/ut5tfu/business/estadistica/impl/EstadisticaServiceImpl.java
@@ -4,30 +4,35 @@ import com.example.ut5tfu.business.estadistica.EstadisticaService;
 import com.example.ut5tfu.model.Itinerario;
 import com.example.ut5tfu.persistence.EstadisticaRepository;
 import com.example.ut5tfu.model.Estadistica;
+import com.example.ut5tfu.web.exception.NotFoundException;
+
 import java.util.List;
 import java.util.Optional;
 
 public class EstadisticaServiceImpl implements EstadisticaService {
 
-
+    private final EstadisticaRepository estadisticaRepository = EstadisticaRepository.getInstance();
 
     public void crearEstadistica(Estadistica estadistica) {
-
+        estadisticaRepository.save(estadistica);
     }
 
     public void actualizarEstadistica(Integer id, Estadistica estadistica) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        estadistica.setId(id);
+        estadisticaRepository.save(estadistica);
     }
 
     public void eliminarEstadistica(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        estadisticaRepository.getById(id)
+                .orElseThrow(() -> new NotFoundException("ESTADISTICA_NO_ENCONTRADA"));
+        estadisticaRepository.delete(id);
     }
 
     public List<Estadistica> obtenerTodos() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return estadisticaRepository.getAll();
     }
 
     public Optional<Estadistica> obtenerPorId(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return estadisticaRepository.getById(id);
     }
 }
